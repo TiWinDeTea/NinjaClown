d99b19e5e8d14b591f315308e459ba0dbdfe403b
Lucas Lazare <lazarelucas@yahoo.fr>
2019-12-23 17:12:56 +0100

    Add utils::synchronized

--- include/utils.hpp
+++ include/utils.hpp
@@ -2,6 +2,7 @@
 #define NINJACLOWN_UTILS_HPP
 
 #include <algorithm>
+#include <mutex>
 #include <string_view>
 
 namespace utils {
@@ -9,6 +10,59 @@ inline bool stars_with(std::string_view str, std::string_view prefix)
 {
 	return std::mismatch(str.begin(), str.end(), prefix.begin(), prefix.end()).second == prefix.end();
 }
+
+template <typename StoredValue, typename Mutex = std::mutex>
+class synchronized {
+
+	template <typename Sync, typename UnderlyingType>
+	struct acquired_impl {
+		explicit acquired_impl(Sync &s) noexcept(noexcept(s.mutex.lock()))
+		    : sync{s}
+		{
+			sync.mutex.lock();
+		}
+		~acquired_impl()
+		{
+			sync.mutex.unlock();
+		}
+
+		[[nodiscard]] UnderlyingType *operator->() noexcept
+		{
+			return &(sync.stored_value);
+		}
+
+		[[nodiscard]] UnderlyingType &operator*() noexcept
+		{
+			return sync.stored_value;
+		}
+
+		Sync &sync;
+	};
+
+public:
+    using acquired_t = acquired_impl<synchronized, StoredValue>;
+    using const_acquired_t = acquired_impl<const synchronized, const StoredValue>;
+
+	template <typename... Args>
+	explicit synchronized(Args &&... args) noexcept(noexcept(Mutex{}) && noexcept(StoredValue{std::forward<Args>(args)...}))
+	    : mutex{}
+	    , stored_value(std::forward<Args>(args)...)
+	{
+	}
+
+    [[nodiscard]] acquired_t acquire() noexcept(noexcept(acquired_t{*this})) {
+        return acquired_t{*this};
+    }
+
+    [[nodiscard]] const_acquired_t acquire() const noexcept(noexcept(const_acquired_t{*this})) {
+        return const_acquired_t{*this};
+    }
+
+private:
+	mutable Mutex mutex;
+	StoredValue stored_value;
+};
+
 } // namespace utils
 
 #endif //NINJACLOWN_UTILS_HPP
