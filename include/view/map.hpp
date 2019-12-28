3e86fc50681abd4c0ebce92d5fb46b1fd99b149b
Lucas Lazare <lazarelucas@yahoo.fr>
2019-12-29 00:04:02 +0100

    Add missing map class

---
+++ include/view/map.hpp
@@ -0,0 +1,13 @@
+#ifndef NINJACLOWN_VIEW_MAP_HPP
+#define NINJACLOWN_VIEW_MAP_HPP
+
+namespace view {
+class map {
+public:
+
+
+private:
+};
+} // namespace view
+
+#endif //NINJACLOWN_VIEW_MAP_HPP
---
+++ src/view/map.cpp
@@ -0,0 +1,1 @@
+#include "view/map.hpp"
