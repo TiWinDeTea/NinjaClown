4536fe2a596c4973ac22935073686e10aee56d37
Benoît CORTIER <benoit.cortier@fried-world.eu>
2019-12-28 20:30:54 -0500

    model: update grid constructor to use cell default constructor

--- src/model/grid.cpp
+++ src/model/grid.cpp
@@ -6,9 +6,6 @@ model::grid::grid(size_t width, size_t height) noexcept
 {
 	m_grid.resize(width);
 	for (auto &column : m_grid) {
-		column.reserve(height);
-		std::generate_n(std::back_inserter(column), height, []() {
-			return cell{cell_type::VOID};
-		});
+		column.resize(height);
 	}
 }
