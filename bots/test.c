4df529260fcb0a1d7f3ed8159585a4e8637be5bb
Benoît CORTIER <benoit.cortier@fried-world.eu>
2020-01-05 16:57:39 -0500

    bot: update test bot

--- bots/test.c
+++ bots/test.c
@@ -4,6 +4,7 @@
 #include "bot_interface/bot.h"
 
 static struct bot_api BOT;
+static int finished = 0;
 
 void NINJACLOWN_DLLEXPORT NINJACLOWN_STDCALL bot_init(struct bot_api api)
 {
@@ -12,22 +13,17 @@ void NINJACLOWN_DLLEXPORT NINJACLOWN_STDCALL bot_init(struct bot_api api)
 
 void NINJACLOWN_DLLEXPORT NINJACLOWN_STDCALL bot_think()
 {
-	BOT.log("hello from bot");
-
-	printf("current x=%f, y=%f, angle=%f\n", BOT.get_x_position(), BOT.get_y_position(), BOT.get_angle());
-
-	if (rand() % 2) {
-		BOT.move_forward();
-		BOT.log("move forward");
+	if (finished) {
+		BOT.log("mission accomplished!");
+		return;
 	}
-	else {
-		if (rand() % 5 == 0) {
-			BOT.activate_button();
-			BOT.log("activate button");
-		}
-		else {
-			BOT.turn_left();
-			BOT.log("turn left");
-		}
+
+	if (BOT.get_angle() < 2.5f) {
+		BOT.turn_left();
+	} else if (BOT.get_x_position() > 7.5) {
+		BOT.move_forward();
+	} else {
+		BOT.activate_button();
+		finished = 1;
 	}
 }
