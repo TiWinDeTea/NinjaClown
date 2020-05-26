014cfd4b46ea8dbcfbb1c185a0d062ed52337acd
Benoît CORTIER <benoit.cortier@fried-world.eu>
2020-05-26 19:15:47 -0400

    bindings/rust: derive more traits with bindgen

--- bindings/rust/ninja_clown_bot_sys/build.rs
+++ bindings/rust/ninja_clown_bot_sys/build.rs
@@ -7,6 +7,11 @@ fn main() {
         .header("wrapper.h")
         .newtype_enum("nnj_.*")
         .derive_default(true)
+        .derive_eq(true)
+        .derive_partialeq(true)
+        .derive_hash(true)
+        .derive_ord(true)
+        .derive_partialord(true)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .size_t_is_usize(true) // we don't expect to target platform where size_t != uintptr_t
         .generate()
