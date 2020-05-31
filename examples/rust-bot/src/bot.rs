4720c0ca7f6a08a38068f6b670061ad1b08e0dfa
Benoît CORTIER <benoit.cortier@fried-world.eu>
2020-05-30 21:03:29 -0400

    examples/rust-bot: update bot to adapt to changing map

--- examples/rust-bot/src/bot.rs
+++ examples/rust-bot/src/bot.rs
@@ -1,8 +1,9 @@
 use crate::path::PathGraph;
 use ninja_clown_bot::{
+    decision::DecisionCommit,
     entity::EntityKind,
     map::{CellPos, InteractionKind},
-    Api, Decision, LogLevel,
+    Api, Decision, Entity,
 };
 use pathfinding::utils::absdiff;
 use std::vec;
@@ -20,8 +21,8 @@ pub struct UserData {
 pub fn init() {}
 
 pub fn start_level(api: &mut Api) -> UserData {
-    api.log(LogLevel::Info, "Bib bop I'm Rust bot");
-    api.log(LogLevel::Info, api.map.to_string());
+    api.log_info("Bib bop I'm Rust bot");
+    api.log_trace(api.map.to_string());
 
     let mut ninja_handle_opt = None;
     for entity in &api.entities {
@@ -50,18 +51,19 @@ pub fn start_level(api: &mut Api) -> UserData {
 pub fn think(api: &mut Api, data: &mut UserData) {
     let ninja_clown = api.entities.get(data.ninja_handle).expect("ninja clown not found");
 
-    if data.current_target.is_none() {
+    // compute path graph
+    if data.current_target.is_none() || api.map.changed() {
         let start = CellPos::new(ninja_clown.x() as usize, ninja_clown.y() as usize);
         let graph = PathGraph::build(&api.map);
 
         let path = if let Some(path) = graph.path_to(&start, &data.target) {
-            api.log(LogLevel::Info, "Moving to the target!");
+            api.log_info("Moving to the target!");
             path
         } else if let Some(path) = graph.path_to(&start, &data.button) {
-            api.log(LogLevel::Info, "Moving to the button!");
+            api.log_info("Moving to the button!");
             path
         } else {
-            api.log(LogLevel::Warn, "Bib bop I'm stuck");
+            api.log_warn("Bib bop I'm stuck");
             return;
         };
 
@@ -77,38 +79,44 @@ pub fn think(api: &mut Api, data: &mut UserData) {
 
     let dist = absdiff(target.center_x(), ninja_clown.x()) + absdiff(target.center_y(), ninja_clown.y());
     if dist < 0.6 {
-        let interaction = api.map.cell_at(target.column(), target.line()).unwrap().interaction();
+        let interaction = api.map.cell_at_pos(target).unwrap().interaction();
         if !data.activated && interaction != InteractionKind::NoInteraction {
             api.commit_decisions(&[Decision::activate(target.column(), target.line()).commit(data.ninja_handle)]);
             data.activated = true;
-        } else {
-            data.current_target = data.path.next();
-            think(api, data)
+        } else if let Some(next_target) = data.path.next() {
+            let decision = move_towards(ninja_clown, &next_target);
+            api.commit_decisions(&[decision]);
+            data.current_target = Some(next_target);
+            data.activated = false;
         }
     } else {
-        let angle = ninja_clown.angle();
-
-        let delta_x = target.center_x() - ninja_clown.x();
-        let delta_y = ninja_clown.y() - target.center_y();
-        let target_angle = delta_y.atan2(delta_x);
-
-        let mut delta_angle = target_angle - angle;
-        while delta_angle > std::f32::consts::PI {
-            delta_angle -= 2.0 * std::f32::consts::PI;
-        }
-        while delta_angle < -std::f32::consts::PI {
-            delta_angle += 2.0 * std::f32::consts::PI;
-        }
-
-        let forward = if delta_angle.abs() > 0.05 {
-            0.1 / delta_angle.abs()
-        } else {
-            ninja_clown.properties().move_speed()
-        };
-
-        let decision = Decision::movement(delta_angle, forward, 0.0);
-        api.commit_decisions(&[decision.commit(data.ninja_handle)]);
+        let decision = move_towards(ninja_clown, target);
+        api.commit_decisions(&[decision]);
     }
+}
+
+fn move_towards(ninja_clown: &Entity, target: &CellPos) -> DecisionCommit {
+    let angle = ninja_clown.angle();
+
+    let delta_x = target.center_x() - ninja_clown.x();
+    let delta_y = ninja_clown.y() - target.center_y();
+    let target_angle = delta_y.atan2(delta_x);
+
+    let mut delta_angle = target_angle - angle;
+    while delta_angle > std::f32::consts::PI {
+        delta_angle -= 2.0 * std::f32::consts::PI;
+    }
+    while delta_angle < -std::f32::consts::PI {
+        delta_angle += 2.0 * std::f32::consts::PI;
+    }
+
+    let forward = if delta_angle.abs() > 0.05 {
+        0.1 / delta_angle.abs()
+    } else {
+        ninja_clown.properties().move_speed()
+    };
+
+    Decision::movement(delta_angle, forward, 0.0).commit(ninja_clown.handle())
 }
 
 pub fn end_level(_api: &mut Api, _data: &mut UserData) {}
