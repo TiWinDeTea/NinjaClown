8acefe9a689e7851f1fc1432af216b07898343e4
Lucas Lazare <lazarelucas@yahoo.fr>
2020-07-05 19:25:39 +0200

    Fixie fouxo

--- src/model/types.hpp
+++ src/model/types.hpp
@@ -4,8 +4,10 @@
 #include <cstdint>
 #include <cstddef>
 
-using tick_t = unsigned int;
-using handle_t = size_t;
+namespace model {
+    using tick_t = unsigned int;
+    using handle_t = unsigned int;
+}
 
 #endif // NINJACLOWN_MODEL_TYPES_HPP_INCLUDED
 
