9c714d81a39be8b870380581a0b820ae1ea072af
Benoît CORTIER <bcortier@proton.me>
2023-10-15 15:01:35 -0400

    Tell msvc to not spawn a terminal and remove wWinMain

--- src/main.cpp
+++ src/main.cpp
@@ -5,49 +5,13 @@
 
 #include "state_holder.hpp"
 
-int actual_main(std::vector<std::string> &args);
-
-#ifdef USE_WINMAIN
-#include <shellapi.h>
-#include <tchar.h>
-#include <windows.h>
-
-INT WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR, INT) {
-
-	int argc;
-	LPWSTR *lpArgv = CommandLineToArgvW(GetCommandLineW(), &argc);
-
-	std::vector<std::string> args;
-	args.reserve(argc);
-	for (int i = 0; i < argc; ++i) {
-		size_t size = wcslen(lpArgv[i]) + 1;
-		std::string arg;
-		arg.resize(size);
-		wcstombs_s(nullptr, arg.data(), arg.size(), lpArgv[i], size);
-		args.emplace_back(std::move(arg));
-	}
-	LocalFree(lpArgv);
-	return actual_main(args);
-}
-#else
-int main(int argc, char *argv[]) {
-	std::vector<std::string> args;
-	args.reserve(argc);
-	while ((argc--) != 0) {
-		args.emplace_back(*argv++);
-	}
-	return actual_main(args);
-}
-#endif
-
-int actual_main([[maybe_unused]] std::vector<std::string> &args) {
+int main() {
 	spdlog::default_logger()->set_level(spdlog::level::trace);
+	// TODO: ajouter un logger à spdlog qui fait des popups pour les erreurs
 
 	state::holder game{"resources/autorun.ncs"};
 	game.run();
 	game.wait();
 
 	return 0;
-
-	// todo : ajouter un logger à spdlog qui fait des popups pour les erreurs
 }
